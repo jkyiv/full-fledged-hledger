d4d6b5b43139b70561e8173cabdb0eb0dc268daa
Dmitry Astapov <dastapov@gmail.com>
2018-04-19 22:39:45 +0100

    moved step 04 from dir to branch

--- src/import/lloyds/lloyds2csv.pl
+++ src/import/lloyds/lloyds2csv.pl
@@ -10,5 +10,6 @@ while (<>) {
   @fields=split(/,/,$_);
   # Clean up account names
   $fields[3] =~ s/99966633/assets:Lloyds:current/;
+  $fields[3] =~ s/12345678/assets:Lloyds:savings/;
   print join(",",@fields)."\n";
 }
